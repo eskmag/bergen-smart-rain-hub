1d0494e0c03ed9ffc3a54a430a2fdffdf6434dd0
Eskil Magnussen <eskil.magnussen@gmail.com>
2026-05-26 18:42:34 +0200

    feat: add LandingNav component

---
+++ frontend-react/src/components/LandingNav.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+export default function LandingNav() {
+  return (
+    <nav className="l-nav">
+      <Link to="/" className="l-nav-logo">
+        <div className="l-nav-logomark">
+          <svg width="16" height="16" fill="none" viewBox="0 0 24 24">
+            <path d="M12 2C8 8 5 12 5 16a7 7 0 0014 0c0-4-3-8-7-14z" fill="white" />
+          </svg>
+        </div>
+        <span className="l-nav-logotype">Bergen Smart Rain Hub</span>
+      </Link>
+      <ul className="l-nav-links">
+        <li><a href="#bakgrunn">Om prosjektet</a></li>
+        <li><a href="#verktoy">Verktøy</a></li>
+        <li><a href="#data">Data</a></li>
+      </ul>
+      <Link to="/beregn" className="l-nav-cta">Beregn ditt bygg</Link>
+    </nav>
+  )
+}
